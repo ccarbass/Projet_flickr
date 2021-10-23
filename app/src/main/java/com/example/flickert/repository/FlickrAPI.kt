211e02018b581a4920c5be98d5e6a3eaad0e2187
Camille <ca.carbasse@orange.fr>
2021-10-23 15:57:46 +0200

    création du repository

--- app/src/main/java/com/example/flickert/repository/FlickrAPI.kt
+++ app/src/main/java/com/example/flickert/repository/FlickrAPI.kt
@@ -1,4 +1,14 @@
 package com.example.flickert.repository
 
+import android.telecom.Call
+import com.example.flickert.model.SearchResult
+import retrofit2.Callback
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface FlickrAPI {
+    @GET("/services/rest?format=json&nojsoncallback=1")
+    fun getInterestingPhotos(@Query("method") method : String,
+                             @Query("api_key") key: String,
+                             @Query("per_page") perpage: String): Callback<SearchResult>
 }
