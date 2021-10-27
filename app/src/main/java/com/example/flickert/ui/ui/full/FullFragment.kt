8773348036f018697304902d161ad266ac7f1414
Camille <ca.carbasse@orange.fr>
2021-10-27 11:58:59 +0200

    design page full

--- app/src/main/java/com/example/flickert/ui/ui/full/FullFragment.kt
+++ app/src/main/java/com/example/flickert/ui/ui/full/FullFragment.kt
@@ -6,7 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.navigation.Navigation
 import com.bumptech.glide.Glide
 import com.example.flickert.R
 
@@ -24,9 +27,17 @@ class FullFragment : Fragment() {
     ): View? {
         val layout=inflater.inflate(R.layout.full_fragment, container, false)
         val image=layout.findViewById<ImageView>(R.id.photofull)
+        val boutonA = layout.findViewById<Button>(R.id.buttonRetourAccueil)
+        val boutonAll=layout.findViewById<Button>(R.id.buttonRetourList)
         val url=FullFragmentArgs.fromBundle(requireArguments()).url
         Glide.with(layout).load(url).into(image)
 
+        boutonA.setOnClickListener{
+            Navigation.findNavController(boutonA).navigate(R.id.full_toMain)
+        }
+        boutonAll.setOnClickListener{
+            Navigation.findNavController(boutonAll).navigate(R.id.full_toList)
+        }
 
         return layout
     }
